23a75afce65db87ba62602041ae1d1b2592a28b3
Mina-Fathy23 <minafathy2013738@gmail.com>
2026-09-03 05:31:23 -0400

    Added improved femtoshell

---
+++ femtoshell/femto_shell.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#define STDIN_FILENO 0
+#define STDOUT_FILENO 1
+
+#define MAX_READ 1024
+#define MAX_SIZE 100
+char buffer[MAX_READ];
+char femto_argv[MAX_SIZE][MAX_SIZE];
+int femto_argc = 0;
+
+void errExit(char* error_msg, int error_num)
+{
+        printf("%s", error_msg);
+        exit(error_num);
+}
+
+int main(int argc, char* argv[])
+{
+	while(1)
+	{
+
+		printf("myshell command > ");	
+		//int read_count = read(STDIN_FILENO, buffer, MAX_READ);
+		if(fgets(buffer, MAX_READ,stdin) == 0)
+			 errExit("read failed\n", -2);
+
+		int temp_index = 0;
+		char temp[MAX_SIZE];
+		memset(temp, 0, MAX_SIZE);
+		for(int i = 0; i < strlen(buffer); i++)
+		{		
+
+		       	if(buffer[i] == ' ' || buffer[i] == '\n')
+			{
+				if(temp_index > 0)
+				{
+					temp[temp_index] = '\0';
+					strcpy(femto_argv[femto_argc], temp);
+              	 		       	femto_argc++;
+					temp_index = 0;
+					memset(temp, 0, MAX_SIZE);
+				}
+				
+			}
+			else
+			{
+	
+				 temp[temp_index] = buffer[i];
+				 temp_index++;
+				
+			}
+
+		}
+
+
+		if(strcmp(femto_argv[0], "echo") == 0)
+		{
+			for(int i = 1; i < femto_argc; i++)
+				printf("%s ", femto_argv[i]);
+			printf("\n");
+		}
+		else if(strcmp(femto_argv[0], "exit") == 0)
+		{
+			printf("Good Bye :)\n");
+			break;
+		}
+		else if(strcmp(femto_argv[0], 0) == 0)
+			continue;
+		else
+			printf("Invalid command\n");
+			
+
+
+		for(int i = 0; i < femto_argc; i++)
+			memset(femto_argv[i], 0, MAX_SIZE);
+		femto_argc = 0;
+	}	
+	exit(0);
+
+
+}
